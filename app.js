bef0a1b17d26d4bb4385ff3632c6c83b5e09e1a9
unknown <ludgerk@techlaunch.com>
2019-03-04 16:29:18 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+class App extends React.Component {
+    render() {
+      return (
+        <div className="app-content">
+          <h1>Hello, World!</h1>
+        </div>
+      )
+    }
+  }
+  ReactDOM.render(<App />, document.getElementById('app'));
